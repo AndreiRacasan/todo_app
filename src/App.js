37ebb7c5ad88d22692bd4f9c6cc74d63516b31f8
Andrei Racasan <aracasan@outlook.com>
2020-12-17 22:22:52 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import List from './comps/List';
+
+function App() {
+  return (
+   <div className="app">
+     <List />
+   </div>
+  );
+}
+
+export default App;
